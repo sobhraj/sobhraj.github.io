482d16e74100f7b666a5b109f1f98a9ffa042fc7
Sobhraj <schhetri@ford.com>
2021-05-15 08:03:11 +0530

    js update

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
-var i = 0;
-var j = 0;
+var i = j = 0;
 var txt = 'Hey there!           Whats up?? ';
 var portfolio = 'Hi!  I\'m Sobhraj Chhetri, AEM Developer based in India.';
 var speed = 50;
-var speed2 = 1000;
+var speed2 = 2000;
 function welcome() {
-var element  = document.getElementById("content");
-element.classList.add("welcome-content");
-if (i == 8) {
- portfolioload();
-}
-  if (i < txt.length) {
-    element.innerHTML += txt.charAt(i);
-    i++;
-  }
-  setTimeout(welcome, speed);
+    var element  = document.getElementById("content");
+    element.classList.add("welcome-content");
+    var timer_port = setTimeout(portfolioload,speed2);
+    if (i < txt.length) {
+        element.innerHTML += txt.charAt(i);
+        i++;
+    }
+    else {
+    clearTimeout(timer_greet);
+    clearTimeout(timer_port);
+    }
+    var timer_greet = setTimeout(welcome, speed);
 }
 
 function portfolioload() {
- var element2  = document.getElementById("intro");
-   element2.classList.add("portfolio-content");
-       element2.innerHTML = 'Hello!  I\'m Sobhraj Chhetri.'
-       element2.innerHTML += '<div class="details"><br/> -  Certified AEM Developer based in India.'
-       + '<br/> - Certified AEM Architect <br/> - Certified AEM DevOps Specialist<br/>- ML/AI Enthusiast<br/>- Freeride Mountain Bike Rider</div>';
-//  }
-  setTimeout(portfolioload, speed2);
+    var element2  = document.getElementById("intro");
+    element2.classList.add("portfolio-content");
+    element2.innerHTML = 'Hello!  I\'m Sobhraj Chhetri.'
+    element2.innerHTML += '<div class="details"><br/> -  Certified AEM Developer based in India.'
+    + '<br/> - Certified AEM Architect <br/> - Certified AEM DevOps Specialist<br/>- ML/AI Enthusiast<br/>- Freeride Mountain Bike Rider</div>';
 }
