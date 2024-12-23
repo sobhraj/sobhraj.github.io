9c7d27e55433a0358427a111821a1ced0cf931ae
Sobhraj <sobhrajchettri@yahoo.com>
2024-12-23 12:27:30 +0530

    Initial Commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <title>Sobhraj Chhetri | Expert AEM Consultant | Transforming Digital Experiences with Seamless Website Development & Optimization</title>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
